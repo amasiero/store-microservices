3af215344831647fb1ad529225624d29793e5234
amasiero <amasiero@tripadvisor.com>
2023-01-27 18:32:18 +0000

    feat: finished initial version of products api

---
+++ products/src/main/java/com/andreymasiero/products/exceptions/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package com.andreymasiero.products.exceptions;
+
+public class ProductNotFoundException extends Exception {
+}
--- products/src/main/java/com/andreymasiero/products/services/ProductService.java
+++ products/src/main/java/com/andreymasiero/products/services/ProductService.java
@@ -1,9 +1,11 @@
 package com.andreymasiero.products.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.andreymasiero.products.dtos.ProductDto;
+import com.andreymasiero.products.entities.Product;
 import com.andreymasiero.products.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,5 +34,24 @@ public class ProductService {
             .stream()
             .map(ProductDto::from)
             .collect(Collectors.toList());
+    }
+
+    public ProductDto findByProductIdentifier(String productIdentifier) {
+        Product product = productRepository.findByProductIdentifier(productIdentifier);
+        return product != null ? ProductDto.from(product) : null;
+    }
+
+    public ProductDto save(ProductDto productDto) {
+        Product product = productRepository.save(Product.from(productDto));
+        return ProductDto.from(product);
+    }
+
+    public ProductDto delete(Long productId) {
+        Optional<Product> product = productRepository.findById(productId);
+        if (product.isPresent()) {
+            productRepository.delete(product.get());
+            return ProductDto.from(product.get());
+        }
+        return null;
     }
 }

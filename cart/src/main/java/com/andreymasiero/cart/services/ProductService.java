44256ec06c6b6e57a8442b1f7ba829562dd2e1f0
Andrey Masiero <andreymasiero@gmail.com>
2023-02-03 00:24:17 +0000

    feat: dockerize it

--- cart/src/main/java/com/andreymasiero/cart/services/ProductService.java
+++ cart/src/main/java/com/andreymasiero/cart/services/ProductService.java
@@ -2,6 +2,7 @@ package com.andreymasiero.cart.services;
 
 import com.andreymasiero.dtos.products.ProductDto;
 import com.andreymasiero.exceptions.products.ProductNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -9,10 +10,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ProductService {
+
+    @Value("${PRODUCT_API_URL:http://localhost:8081/product/}")
+    private String productUrlApi;
+
     public ProductDto getProductByIdentifier(String productIdentifier) {
         try {
             RestTemplate restTemplate = new RestTemplate();
-            String url = "http://localhost:8081/product/" + productIdentifier;
+            String url = productUrlApi + productIdentifier;
             ResponseEntity<ProductDto> response = restTemplate.getForEntity(url, ProductDto.class);
             return response.getBody();
         } catch (HttpClientErrorException.NotFound e) {
--- cart/src/main/java/com/andreymasiero/cart/services/UserService.java
+++ cart/src/main/java/com/andreymasiero/cart/services/UserService.java
@@ -2,6 +2,7 @@ package com.andreymasiero.cart.services;
 
 import com.andreymasiero.dtos.users.UserDto;
 import com.andreymasiero.exceptions.users.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -9,10 +10,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
+
+    @Value("${USER_API_URL:http://localhost:8080/user/social-id/}")
+    private String userUrlApi;
+
     public UserDto getUserBySocialId(String socialId) {
         try {
             RestTemplate restTemplate = new RestTemplate();
-            String url = "http://localhost:8080/user/social-id/" + socialId;
+            String url = userUrlApi + socialId;
             ResponseEntity<UserDto> response = restTemplate.getForEntity(url, UserDto.class);
             return response.getBody();
         }catch (HttpClientErrorException.NotFound e) {
